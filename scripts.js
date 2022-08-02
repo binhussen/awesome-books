ca0051d8ecfc23ef61288b6d2c635428bf2bdd7c
binhussen <mhdhsna@gmail.com>
2022-08-02 14:34:13 +0300

    Update Display function

--- scripts.js
+++ scripts.js
@@ -1,51 +1,61 @@
 /** @format */
 /** @format */
 
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const submit = document.querySelector("#submit");
-const container = document.querySelector(".data-container");
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const submit = document.querySelector('#submit');
+const container = document.querySelector('.data-container');
 
-const books = [];
+let books = [];
 
-setLocalStorage = (books) =>
-	localStorage.setItem("books", JSON.stringify(books));
+const setLocalStorage = (books) =>
+  localStorage.setItem('books', JSON.stringify(books));
 
-getFromLocalStorage = () => JSON.parse(localStorage.getItem("booksInfo"));
+const getFromLocalStorage = () => {
+  if (JSON.parse(localStorage.getItem('books')))
+    books = JSON.parse(localStorage.getItem('books'));
+};
 
-displayBooks = () => {
-	container.innerHTML = "";
-	books.forEach((book) => {
-		container.innerHTML += `<div class="book">
+const displayBooks = () => {
+  getFromLocalStorage();
+  container.innerHTML = '';
+  books.forEach((book) => {
+    container.innerHTML += `<div class="book">
         <p><strong>${book.title}</strong></p>
         <p><strong>${book.author}</strong></p>
         <input type="button" value="Remove" onclick="removeBook(${books.indexOf(
-					book
-				)})">
+          book
+        )})">
         </div>
         <hr/>`;
-	});
+  });
 };
 
-addBook = (e) => {
-	e.preventDefault();
-	const newBook = {
-		title: title.value,
-		author: author.value,
-	};
-	books.push(newBook);
-	clear();
-	displayBooks(books);
+const clear = () => {
+  title.value = '';
+  author.value = '';
 };
 
-removeBook = (i) => {
-	books.filter((book) => book !== books[i]);
-	displayBooks();
+const addBook = (e) => {
+  e.preventDefault();
+  const newBook = {
+    title: title.value,
+    author: author.value,
+  };
+  books.push(newBook);
+  clear();
+  setLocalStorage(books);
+  displayBooks();
 };
 
-clear = () => {
-	title.value = "";
-	author.value = "";
+const removeBook = (i) => {
+  const filteredBooks = books.filter((book) => book !== books[i]);
+  setLocalStorage(filteredBooks);
+  displayBooks();
 };
 
-submit.addEventListener("click", addBook);
+submit.addEventListener('click', addBook);
+
+document.addEventListener('DOMContentLoaded', () => {
+  displayBooks();
+});
