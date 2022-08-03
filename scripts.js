9b6ed59b3bf9f8f001465b0737414f4a4acc3b1f
binhussen <mhdhsna@gmail.com>
2022-08-03 12:18:53 +0300

    style table to view

--- scripts.js
+++ scripts.js
@@ -28,7 +28,7 @@ class Books {
     this.books.forEach((book, i) => {
       container.innerHTML += `<div class="book">
           <p><q>${book.title}</q> by ${book.author}</p>
-          <button class="remove" id=${i}>Remove</button>
+          <button class="remove btn" id=${i}>Remove</button>
         </div>`;
     });
   };
